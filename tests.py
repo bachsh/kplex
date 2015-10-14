6301311b3499962954363c3a229a6bb8950148e4
aaminov <aaminov@akamai.com>
2015-10-14 14:53:39 +0300

    Started writing analysis for c-elegans network

--- tests.py
+++ tests.py
@@ -3,8 +3,11 @@ from kplex import kplexAlg
 import matplotlib.pyplot as plt
 from time import time
 import pandas as pd
+import pickle
+from os.path import isfile
 
 def simpleExample():
+    print "running simple example"
     G = nx.Graph({
         0: {1,2},
         1: {0, 4,5},
@@ -15,18 +18,18 @@ def simpleExample():
     })
     N = len(G.nodes())
     k = 2
-    kplexes = kplexAlg(G, k)
-    print "List of {}-plexes".format(k)
-    print kplexes
+
+    analyzeNetwork(G, k, "simple")
+    # kplexes = kplexAlg(G, k)
 
 
 def timings():
-    N_range = [5, 10, 30, 50, 100, 200, 500]
-    # N_range = [5,10]
+    N_range = [5, 10, 30, 50, 100, 200, 500, 1000, 10000]
+    N_range = [5, 10, 30, 50, 100, 200]
     df = pd.DataFrame({}, columns=["N", "time"])
     for N in N_range:
         print N
-        G = nx.fast_gnp_random_graph(N, 0.3)
+        G = nx.fast_gnp_random_graph(N, 10./N)
         start_time = time()
         kplexAlg(G, 2)
         run_time = time()-start_time
@@ -38,7 +41,33 @@ def timings():
 
 
 
+def analyzeNetwork(G, k=2, filename=None):
+    # Get kplexes
+    if filename and isfile(filename):
+        kplexesMax = pickle.load(open(filename))
+    else:
+        _, kplexesMax = kplexAlg(G, k, verbose=True)
+        if filename:
+            pickle.dump(kplexesMax, open(filename, 'wb'))
+
+    print "List of {}-plexes".format(k)
+    print kplexesMax
+
+    # get histogram for size of kplex
+    kplexSizes = map(len, kplexesMax)
+    plt.hist(kplexSizes)
+    # get MCC size...
+
+
+def c_elegans():
+    print "Reading worm..."
+    G = nx.read_gml("Networks/CelegansNeural/celegansneural.gml")
+    print "Done Reading worm. Worm has {} nodes and {} edges".format(len(G.nodes()), len(G.edges()))
+    analyzeNetwork(G, 2, "worm")
+
+
 if __name__ == "__main__":
     # simpleExample()
-    timings()
+    # timings()
+    c_elegans()
 
